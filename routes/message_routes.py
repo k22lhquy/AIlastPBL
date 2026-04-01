5975bc6c449d4a570a2a96d656d75b13e8c3a285
Quy le <quyle150802@gmail.com>
2026-04-02 01:23:02 +0700

    all message by chatId

--- routes/message_routes.py
+++ routes/message_routes.py
@@ -1,16 +1,20 @@
 from email import message
 
 from fastapi import APIRouter, Depends, HTTPException
-from controllers.message_controller import message_controller
+from controllers.message_controller import message_controller, get_messages_controller
 from middlewares.auth_middleware import get_current_user
 from pydantic import BaseModel
 
 class SendMessageRequest(BaseModel):
     message: str
     conversationId: str
-
+    
 router = APIRouter()
 
 @router.post("/send_message")
 async def send_message(user=Depends(get_current_user), request: SendMessageRequest = None):
     return await message_controller(user_id=user["user_id"], message=request.message, conversationId=request.conversationId)
+
+@router.get("/all_messages/{conversationId}")
+async def get_messages(user=Depends(get_current_user), conversationId: str = None):
+    return await get_messages_controller(user_id=user["user_id"], conversationId=conversationId)
--- services/message_service.py
+++ services/message_service.py
@@ -22,5 +22,13 @@ async def message_service(user_id: str, message: str, conversationId: str):
         "message": message,
         "conversationId": conversationId
     }
-    
+
+async def get_messages_service(user_id: str, conversationId: str):
+    messages_cursor = message_collection.find({"conversationId": conversationId}).sort("timestamp", 1)
+    messages = []
+    async for message in messages_cursor:
+        message["id"] = str(message["_id"])
+        del message["_id"]
+        messages.append(message)
+    return messages
 # chỉnh lại return có status code và message rõ ràng hơn, có thể trả về id của message mới tạo để client dễ dàng quản lý sau này.
